42b1099e03d96ad50d5adf47c4d79190ce8e40f8
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-31 17:32:31 -0600

    Updates

---
+++ precache-manifest.3d9c7974f16c51292ea8c54df146d577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf58732602fa3442a33cb3df1e9fd400",
+    "url": "/coenCollection/static/media/wood.cf587326.jpg"
+  },
+  {
+    "revision": "4bcdb77d331057362271185bc2b8ed81",
+    "url": "/coenCollection/static/media/circle-loader.4bcdb77d.gif"
+  },
+  {
+    "revision": "054582b0f0d4f0fe70e2",
+    "url": "/coenCollection/static/js/runtime~main.054582b0.js"
+  },
+  {
+    "revision": "5a094f42c7a5286d8627",
+    "url": "/coenCollection/static/js/main.5a094f42.chunk.js"
+  },
+  {
+    "revision": "cf74c6972840e16e3699",
+    "url": "/coenCollection/static/js/1.cf74c697.chunk.js"
+  },
+  {
+    "revision": "5a094f42c7a5286d8627",
+    "url": "/coenCollection/static/css/main.ec6dc164.chunk.css"
+  },
+  {
+    "revision": "b1a868856b5b67cfccde0f3d99b461f8",
+    "url": "/coenCollection/index.html"
+  }
+];
